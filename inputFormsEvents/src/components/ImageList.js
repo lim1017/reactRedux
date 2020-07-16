280e3bb0c5ab37894ae542f421f0ef724fe96a95
tom <lim1017@hotmail.com>
2020-07-16 14:51:11 -0400

    done

--- inputFormsEvents/src/components/ImageList.js
+++ inputFormsEvents/src/components/ImageList.js
@@ -2,13 +2,21 @@ import React from 'react'
 
 class ImageInList extends React.Component {
 
+  constructor(props){
+    super(props)
+
+    this.imageRef=React.createRef();
+  }
+
+
   render() { 
+    console.log(this.imageRef)
 
     const {id, urls} =this.props.img
-
     return ( 
       
       <img
+      ref={this.imageRef}
       key={id}
       src={urls.regular}
       alt="Girl in a jacket"
