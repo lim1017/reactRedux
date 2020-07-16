1ab17dc89d3f138a448e3f3809b45d940bc36c69
tom <lim1017@hotmail.com>
2020-07-15 23:21:52 -0400

    api request successful

--- inputFormsEvents/src/App.js
+++ inputFormsEvents/src/App.js
@@ -1,19 +1,38 @@
 import React from 'react'
 import SearchBar from './components/SearchBar'
+import axios from 'axios'
 
 class App extends React.Component {
   
-  state = { searchText: "" }
+  state = { searchText: "", searchResults:[] }
   
   onFormSubmit = (e, value) =>{
     e.preventDefault()
-    alert(`submitted ${value}`)
+
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers:{
+        Authorization: 'Client-ID JELNO8PZyJFYAFgGYtVbTgqmS_hAoSMnB4puJQaPXMs'
+      },
+      params:{
+        query: value
+      }
+    }).then(response =>{
+      console.log(response)
+      this.setState({searchResults: response.data.results})
+    })
+
   }
 
   render(){
     return (
       <div style={{display:"flex", justifyContent:"center", marginTop:"2em"}}>
         <SearchBar onFormSubmit={this.onFormSubmit} />
+
+        {this.state.searchResults.map((img) =>{
+          return (
+          <img src={`${img.urls.regular}`} alt="Girl in a jacket" width="500" height="600"></img>
+          )
+        })}
       </div>
     )
   }
