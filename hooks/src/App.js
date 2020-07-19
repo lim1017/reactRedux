f77f8b7df76cc3b5e4fbdaf05ea8003702f3dd6c
tom <lim1017@hotmail.com>
2020-07-19 19:29:55 -0400

    adding non react router nav

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,61 +1,82 @@
 import React, { useState } from "react";
-import './App.css';
-import QuestionsContainer from "./components/QuestionsContainer"
-import SearchContainer from "./components/SearchContainer"
-import DropDownContainer from "./components/DropDownContainer"
-import Tranlate from "./components/Translate"
-
-
+import "./App.css";
+import QuestionsContainer from "./components/QuestionsContainer";
+import SearchContainer from "./components/SearchContainer";
+import DropDownContainer from "./components/DropDownContainer";
+import Tranlate from "./components/Translate";
 
 const items = [
-  { 
+  {
     title: "What is React?",
-    content: "React is a JS library"
+    content: "React is a JS library",
   },
-  { 
+  {
     title: "What is Angular?",
-    content: "Angular is a JS library"
+    content: "Angular is a JS library",
   },
-  { 
+  {
     title: "What is Vue?",
-    content: "Vue is a JS library"
+    content: "Vue is a JS library",
   },
-  { 
+  {
     title: "Which one is the best one??",
-    content: "React is a JS library"
+    content: "React is a JS library",
   },
-]
+];
 
 const options = [
   {
     label: "Color Red",
-    value: 'red',
+    value: "red",
   },
   {
     label: "Color Green",
-    value: 'green',
+    value: "green",
   },
   {
     label: "Color Blue",
-    value: 'blue',
+    value: "blue",
   },
-]
+];
+
+
 
 function App() {
-
   const [color, setColor] = useState(options[0]);
+
+
+  const showWidgets = () => {
+    if (window.location.pathname === "/") {
+      return <QuestionsContainer items={items} />;
+    }
+  
+    if (window.location.pathname === "/search") {
+      return <SearchContainer />;
+    }
+  
+    if (window.location.pathname === "/dropdown") {
+      return (
+        <DropDownContainer
+          type="Color"
+          options={options}
+          selected={color}
+          setSelected={setColor}
+        />
+      );
+    }
+  
+    if (window.location.pathname === "/translate") {
+      return <Tranlate />;
+    }
+  };
 
 
   return (
     <div className="App">
-     <div>hooks</div>
-
-     <QuestionsContainer items={items} />
-     <SearchContainer />
-     <DropDownContainer type='Color' options={options} selected={color} setSelected={setColor} />
-     <Tranlate />
+      <div>hooks</div>
+      {showWidgets()}
     </div>
-  );  
+  );
 }
 
 export default App;
---
+++ hooks/src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react";
+
+
+const Convert =({lang, text}) =>{
+ 
+  return (
+    <div>
+      Header 
+    </div>
+ )
+}
+
+export default Convert;
