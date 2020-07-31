9dad70e5bb37287ca0860b5c41ad2b43059b5354
tom <lim1017@hotmail.com>
2020-07-31 01:15:28 -0400

    user auth completed

--- twitch/src/components/GoogleAuth.js
+++ twitch/src/components/GoogleAuth.js
@@ -1,62 +1,68 @@
-import React from 'react'
+import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
 
 class GoogleAuth extends React.Component {
 
-  state = { isSignedIn: null}
-
-
-  componentDidMount(){
-    window.gapi.load('client:auth2', ()=>{
-      window.gapi.client.init({
-        clientId:"424306628076-1j4j3kfrgg5icd7lit6im8q2vr9ta39f.apps.googleusercontent.com",
-        scope: "email"
-      }).then(()=>{
-        this.auth = window.gapi.auth2.getAuthInstance()
-        this.setState({ isSignedIn: this.auth.isSignedIn.get()})
-        this.auth.isSignedIn.listen(this.onAuthChg)
-      })
-
-
-    }) 
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "424306628076-1j4j3kfrgg5icd7lit6im8q2vr9ta39f.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChg(this.auth.isSignedIn.get())
+          this.auth.isSignedIn.listen(this.onAuthChg);
+        });
+    });
   }
 
-  onAuthChg = () =>{
-    this.setState({isSignedIn: this.auth.isSignedIn.get()})
-  }
+  onAuthChg = (isSignedIn) => {
+    if (isSignedIn){
+      this.props.signIn(this.auth.currentUser.get().getId())
+    } else this.props.signOut()
 
-  onSignIn=()=>{
-    this.auth.signIn()
-  }
+  };
 
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
 
-  onSignOut=()=>{
-    this.auth.signOut()
-  }
-  
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
 
-  renderSignedIn () {
-    if (this.state.isSignedIn === null){
-      return null
-    } else if( this.state.isSignedIn){
+  renderSignedIn() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Logout
         </button>
-      ) 
+      );
     } else {
       return (
-        <button onClick={this.onSignIn} className="ui red google button">
+        <button onClick={this.onSignInClick} className="ui red google button">
           <i className="google icon" />
           Login
         </button>
-      ) 
+      );
     }
   }
 
-  render (){
-    return <div>{this.renderSignedIn()}</div>
+  render() {
+    console.log(this.props)
+    return <div>{this.renderSignedIn()}</div>;
   }
 }
 
-export default GoogleAuth
+const mapStateToProps = (state) =>{
+  return { isSignedIn: state.auth.isSignedIn, userId:state.auth.userId }
+}
+
+export default connect(mapStateToProps, {signIn, signOut})(GoogleAuth);
--- twitch/src/index.js
+++ twitch/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+
+
+import App from './App';
+import reducers from './reducers'
+
+const store= createStore(reducers)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
